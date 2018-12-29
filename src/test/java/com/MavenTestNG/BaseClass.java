6e6285a4eab948b8531dd54d737f8efe6f8b78cd
blackhonda2015 <blackhonda2015@gmail.com>
2018-12-29 11:53:56 -0500

    updated

--- src/test/java/com/MavenTestNG/BaseClass.java
+++ src/test/java/com/MavenTestNG/BaseClass.java
@@ -1,5 +1,6 @@
 package com.MavenTestNG;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -11,6 +12,8 @@ public class BaseClass {
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
+		
+		driver.findElement(By.xpath("//input[@title='Search']")).sendKeys("syntax");
 	}
 
 }
